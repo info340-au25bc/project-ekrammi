e65392b4047f574e4359f9b79f9d8b5b7496e37d
ekrammi <ekrammilash3@gmail.com>
2025-11-20 21:15:06 -0800

    added schedule content

---
+++ src/schedule.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+const pilatesSchedule = [
+    {id: 1, day: "Monday", time: "9:00 AM", type: "Matt"},
+    {id: 2, day: "Tuesday", time: "11:00 AM", type: "Matt"},
+    {id: 3, day: "Wednsday", time: "9:00 AM", type: "Matt"},
+    {id: 4, day: "Thursday", time: "5:00 PM", type: "Matt"},
+    {id: 5, day: "Friday", time: "6:00 PM", type: "Matt"},
+    {id: 6, day: "Saturday", time: "8:00 AM", type: "Matt"},
+    {id: 7, day: "Sunday", time: "10:00 AM", type: "Matt"},
+    {id: 8, day: "Monday", time: "12:00 PM", type: "Matt"},
+    {id: 9, day: "Tuesday", time: "9:00 AM", type: "Matt"},
+    {id: 10, day: "Wednsday", time: "8:00 AM", type: "Matt"}
+
+]
+
+function Schedule(){
+    return <h1> Schedule page </h1>;
+}
+
+export default function Schedule(){
+    return <Schedule />;
+}
